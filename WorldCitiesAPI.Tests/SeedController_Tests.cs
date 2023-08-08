53107705bcd0addbbf24e66d52f4be854e2e5b30
Forrest Boyer <forrestboyer2@gmail.com>
2023-08-07 21:04:42 -0400

    added authorization support

---
+++ WorldCitiesAPI.Tests/SeedController_Tests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System.Threading.Tasks;
+using WorldCitiesAPI.Controllers;
+using WorldCitiesAPI.Data;
+using WorldCitiesAPI.Data.Models;
+using Xunit;
+
+namespace WorldCitiesAPI.Tests
+{
+    public class SeedController_Tests
+    {
+        /// <summary>
+        /// Test the CreateDefaultUsers() method
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task CreateDefaultUsers()
+        {
+            // ARRANGE
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "WorldCities")
+                .Options;
+
+            var mockEnv = Mock.Of<IWebHostEnvironment>();
+
+            var mockConfiguration = new Mock<IConfiguration>();
+            mockConfiguration.SetupGet(x => x[It.Is<String>(s => s == "DefaultPasswords:RegisteredUser")]).Returns("M0ckPa$$word");
+            mockConfiguration.SetupGet(x => x[It.Is<String>(s => s == "DefaultPasswords:Administrator")]).Returns("M0ckPa$$word");
+
+            using var context = new ApplicationDbContext(options);
+
+            var roleManager = IdentityHelper.GetRoleManager(new RoleStore<IdentityRole>(context));
+
+            var userManager = IdentityHelper.GetUserManager(new UserStore<ApplicationUser>(context));
+
+            var controller = new SeedController(
+                    context,
+                    mockEnv,
+                    roleManager,
+                    userManager,
+                    mockConfiguration.Object
+                );
+
+            ApplicationUser user_Admin = null!;
+            ApplicationUser user_User = null!;
+            ApplicationUser user_NotExisting = null!;
+
+            // ACT
+            await controller.CreateDefaultUsers();
+
+            user_Admin = await userManager.FindByEmailAsync("admin@email.com");
+            user_User = await userManager.FindByEmailAsync("user@email.com");
+            user_NotExisting = await userManager.FindByEmailAsync("notexisting@email.com");
+
+            // ASSERT
+            Assert.NotNull(user_Admin);
+            Assert.NotNull(user_User);
+            Assert.Null(user_NotExisting);
+        }
+    }
+}
--- WorldCitiesAPI/Program.cs
+++ WorldCitiesAPI/Program.cs
@@ -3,9 +3,15 @@ using WorldCitiesAPI.Data;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.MSSqlServer;
+using WorldCitiesAPI.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
+// Create Builder
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Loggin Functionality
 builder.Host.UseSerilog((ctx, lc) => lc
     .ReadFrom.Configuration(ctx.Configuration)
     .WriteTo.MSSqlServer(
@@ -19,18 +25,51 @@ builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console());
 
 // Add services to the container.
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<JwtHandler>();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.WriteIndented = true;
     });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add Authentication Services and Middleware
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecurityKey"]))
+    };
+});
+
+// Build App and configure app Middleware
 var app = builder.Build();
 
 app.UseSerilogRequestLogging();
@@ -44,6 +83,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
